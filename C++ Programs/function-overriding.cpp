9362b215c6b663bcc64c7382a62ba7cc86920fec
Restyled.io <commits@restyled.io>
2021-10-01 03:55:53 +0000

    Restyled by astyle

--- C++ Programs/function-overriding.cpp
+++ C++ Programs/function-overriding.cpp
@@ -1,28 +1,28 @@
 #include<iostream>
 using namespace std;
 
-class base{
-    public:
-    void display(){
+class base {
+public:
+    void display() {
         cout<<" THIS IS Base CLASS"<<endl;
-    } 
+    }
 };
 
-class derived : public base{        //Inheriting base class 
-     public:
-  void display(){
+class derived : public base {       //Inheriting base class
+public:
+    void display() {
         cout<<" THIS IS Derived CLASS"<<endl;
-    } 
+    }
 
 };
 
-int main(){
- 
- base obj1;         ////CREATING A BASE OBJECT
- obj1.display();
+int main() {
 
-  derived obj2;     ////CREATING A DERIVED OBJECT
-  obj2.display();      
+    base obj1;         ////CREATING A BASE OBJECT
+    obj1.display();
+
+    derived obj2;     ////CREATING A DERIVED OBJECT
+    obj2.display();
 
     return 0;
 }
